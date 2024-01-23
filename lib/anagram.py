9ced9aff8da0b262d6a77884f486cdd5cc6d17a7
Boniface <cheruiyotbony@gmail.com>
2024-01-23 10:54:13 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,25 @@
-# your code goes here!
+# your code goes here
+
+class Anagram:
+    
+    def __init__(self,word):
+        
+        self.word=word.lower()
+        
+        
+    def match(self,data):
+        
+        return [n for n in data if  self.is_match(n)]
+    
+    
+    def is_match(self,candidate):
+        
+        candidate=candidate.lower()
+        
+        return sorted(self.word) == sorted(candidate)
+    
+
+listen=Anagram("Boniface")
+
+result=listen.match(['enlists', 'google', 'inlets', 'banana'])
+print(result)
